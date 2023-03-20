a1dc10d87ca86a5f56687fb4f9418be8324dd420
Vladislav Razin <razin36986@gmail.com>
2023-03-20 13:42:51 +0100

    refactor: remove console.log from aidbox-client.ts

--- aidbox-complex-query/src/aidbox-client.ts
+++ aidbox-complex-query/src/aidbox-client.ts
@@ -1,7 +1,5 @@
 import {Client} from "aidbox-sdk";
 
-console.log(import.meta)
-
 export const aidboxClient = new Client(import.meta.env.VITE_AIDBOX_URL, {
     username: import.meta.env.VITE_AIDBOX_USERNAME,
     password: import.meta.env.VITE_AIDBOX_SECRET
