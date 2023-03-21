6619e924a72ae29e28c5c41de19485f200c724cb
Vladislav Razin <razin36986@gmail.com>
2023-03-21 11:27:21 +0100

    refactor: app.tsx

--- aidbox-complex-query/src/app.tsx
+++ aidbox-complex-query/src/app.tsx
@@ -1,42 +1,43 @@
-import { Pagination, Table } from '@nextui-org/react';
-import { aidboxClient } from "./aidbox-client";
-import {useCallback, useEffect, useState} from "react";
-import { Practitioner } from "aidbox-sdk/aidbox-types";
-import { ExecuteQueryResponseWrapper } from "aidbox-sdk";
-import "./app.css"
+import { Loading, Pagination, Table } from '@nextui-org/react'
+import { ExecuteQueryResponseWrapper } from 'aidbox-sdk'
+import { Practitioner } from 'aidbox-sdk/aidbox-types'
+import { useCallback, useEffect, useState } from 'react'
+
+import { aidboxClient } from './aidbox-client'
+
+import './app.css'
 
 const columns: { key: keyof Item, label: string }[] = [
-    {
-        key: "name",
-        label: "Name",
-    },
-    {
-        key: "next_week_appointments",
-        label: "Appointments Next Week"
-    },
-    {
-        key: "appointments_for_three_month",
-        label: "Three Month Average",
-    }
-];
+  {
+    key: 'name',
+    label: 'Name'
+  },
+  {
+    key: 'next_week_appointments',
+    label: 'Appointments Next Week'
+  },
+  {
+    key: 'appointments_for_three_month',
+    label: 'Three Month Average'
+  }
+]
 
-
-type Item =  {
-    id: string
-    name?: string
-    next_week_appointments?: number,
-    appointments_for_three_month?: number,
+type Item = {
+  id: string
+  name?: string
+  next_week_appointments?: number,
+  appointments_for_three_month?: number,
 }
 
 interface ResponseItem {
-    practitioner: Practitioner,
-    appointments_for_three_month: number,
-    next_week_appointments: number
+  practitioner: Practitioner,
+  appointments_for_three_month: number,
+  next_week_appointments: number
 }
 
 type Response = ResponseItem[]
 
-const APPOINTMENT_START_INDEX = `create index on appointment ((appointment.resource #>> '{ start }'));`
+const APPOINTMENT_START_INDEX = 'create index on appointment ((appointment.resource #>> \'{ start }\'));'
 
 const APPOINTMENT_PRACTITIONER_ID_INDEX = `
     create index on appointment (
@@ -61,106 +62,120 @@ const QUERY = `
     ) as next_week_appointments
     from practitioner limit 10 offset {{params.offset}};`
 
-
-const COUNT_QUERY = `select count(*) from practitioner`
+const COUNT_QUERY = 'select count(*) from practitioner'
 
 const queryOptions = {
-    params: {
-        offset: {
-            default: 0,
-            type: "number",
-            isRequired: false
-        }
-    },
-    query: QUERY,
-    "count-query": COUNT_QUERY
+  'params': {
+    offset: {
+      default: 0,
+      type: 'number',
+      isRequired: false
+    }
+  },
+  'query': QUERY,
+  'count-query': COUNT_QUERY
 }
 
+export function App () {
+  let [rows, setRows] = useState<Item[]>()
+  let [loading, setLoading] = useState<boolean>(true)
+  let [total, setTotal] = useState<number>()
+  let [page, setPage] = useState<number>(1)
+  let [initialized, setInitialized] = useState(false)
 
-export function App() {
-    let [rows, setRows] = useState<Item[]>()
-    let [loading, setLoading] = useState<boolean>(true)
-    let [total, setTotal] = useState<number>()
-    let [page, setPage] = useState<number>(1)
-    let [ initialized, setInitialized ] = useState(false)
+  let initializeQuery = useCallback(() => {
+    return Promise.all([
 
-    function initializeQuery() {
-        return Promise.all([
-            aidboxClient.rawSQL(APPOINTMENT_START_INDEX),
-            aidboxClient.rawSQL(APPOINTMENT_PRACTITIONER_ID_INDEX),
-            aidboxClient.createQuery('dashboard-query', queryOptions)
-        ])
-    }
+      // Creating index on field "start"
+      aidboxClient.rawSQL(APPOINTMENT_START_INDEX),
 
-    let setData = useCallback((rawData: ExecuteQueryResponseWrapper<Response>) => {
-        const mappedData = rawData.data.map((resource) => {
-            return {
-                id: resource.practitioner.id ?? '',
-                name: resource.practitioner?.name?.[0].family + " " + (resource.practitioner?.name?.[0]?.given?.[0] ?? ''),
-                appointments_for_three_month: resource.appointments_for_three_month,
-                next_week_appointments: resource.next_week_appointments
-            }
-        })
-        setTotal(rawData.total)
-        setRows(mappedData)
-        setLoading(false)
-    }, [])
+      // Creating index on practitioner id
+      aidboxClient.rawSQL(APPOINTMENT_PRACTITIONER_ID_INDEX),
 
+      // Saving query as resource to call it later
+      aidboxClient.createQuery('dashboard-query', queryOptions)
+    ])
+  }, [])
 
-    useEffect(() => {
-        if (!initialized) {
-            initializeQuery()
-                .finally(() => setInitialized(true))
+  let setData = useCallback((rawData: ExecuteQueryResponseWrapper<Response>) => {
+    let mappedData = rawData.data.map((resource) => {
+      return {
+        id: resource.practitioner.id ?? '',
+        name: resource.practitioner?.name?.[0].family + ' ' + (resource.practitioner?.name?.[0]?.given?.[0] ?? ''),
+        appointments_for_three_month: resource.appointments_for_three_month,
+        next_week_appointments: resource.next_week_appointments
+      }
+    })
+    setTotal(rawData.total)
+    setRows(mappedData)
+    setLoading(false)
+  }, [])
 
-            return
-        }
+  let getData = useCallback(async () => {
+    setLoading(true)
+    let response = await aidboxClient.executeQuery<Response>('dashboard-query', { offset: 10 * (page - 1) })
+    setData(response.data)
+  }, [page, setData])
 
-        setLoading(true)
-        aidboxClient.executeQuery<Response>('dashboard-query', {offset: 10 * (page - 1)})
-            .then((response) => {
-                setData(response.data)
-            })
-    },[initialized, page])
-
-
+  useEffect(() => {
     if (!initialized) {
-        return <div>loading...</div>
+      initializeQuery()
+        .finally(() => setInitialized(true))
+
+      return
     }
 
-    return (
-        <div className='container'>
-            <Table
-                aria-label="Doctor Employment Table"
-                css={{
-                    height: "auto",
-                    minWidth: "100%",
-                }}>
+    getData()
+      .catch((e) => {
+        console.error(e)
+      })
+  }, [getData, initializeQuery, initialized])
 
-                <Table.Header columns={columns}>
-                    {(column) => (
-                        <Table.Column key={column.key}>{column.label}</Table.Column>
-                    )}
-                </Table.Header>
+  if (!initialized) {
+    return <Loading>Loading...</Loading>
+  }
 
-                <Table.Body
-                    items={rows ?? []}
-                    loadingState={loading ? 'loading' : undefined }
-                >
-                    {(row) => (
-                        <Table.Row key={row.id}>
-                            {/*@ts-ignore*/}
-                            {(columnKey) => <Table.Cell>{row?.[columnKey] ?? ''}</Table.Cell>}
-                        </Table.Row>
-                    )}
-                </Table.Body>
+  return (
+    <div className='container'>
+      <Table
+        aria-label='Doctor Employment Table'
+        css={{
+          height: 'auto',
+          minHeight: '10rem',
+          minWidth: '100%'
+        }}
+      >
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column
+              align='start'
+              key={column.key}
+            >{column.label}
+            </Table.Column>
+          )}
+        </Table.Header>
 
-            </Table>
+        <Table.Body
+          items={rows ?? []}
+          loadingState={loading ? 'loading' : undefined }
+        >
+          {(row) => (
+            <Table.Row key={row.id}>
+              {(columnKey) => <Table.Cell>{row?.[columnKey as keyof Item] ?? ''}</Table.Cell>}
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
 
-            {total && (
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Pagination total={Math.ceil(total / 10)} initialPage={1} onChange={(page) => setPage(page)} />
-                </div>
-            )}
+      {total && (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Pagination
+            total={Math.ceil(total / 10)}
+            initialPage={1}
+            onChange={setPage}
+          />
         </div>
-    )
+      )}
+    </div>
+  )
 }
