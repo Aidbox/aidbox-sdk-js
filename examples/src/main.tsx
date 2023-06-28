c49bcaae50479ad73abed0fe7916f5d4f44aa9a2
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2023-06-28 14:21:42 +0200

    chore(subscriptions-ui): some cosmetic changes

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -1,52 +1,52 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import { App as QuerySample } from "../aidbox-querying/src/app";
-import { App as SubsSample } from "../aidbox-subscription/frontend/app";
-import { App as Phr } from "../personal-health-record/src/app";
+import { App as QuerySample } from '../aidbox-querying/src/app'
+import { App as SubsSample } from '../aidbox-subscription/frontend/app'
+import { App as Phr } from '../personal-health-record/src/app'
 
-import App from "./App";
-import "./index.css";
-import { Layout } from "./Layout";
+import App from './App'
+import './index.css'
+import { Layout } from './Layout'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: (
       <Layout>
         <App />
       </Layout>
-    ),
+    )
   },
   {
-    path: "phr",
+    path: 'phr',
     element: (
       <Layout>
         <Phr />
       </Layout>
-    ),
+    )
   },
   {
-    path: "complex-query",
+    path: 'complex-query',
     element: (
       <Layout>
         <QuerySample />
       </Layout>
-    ),
+    )
   },
   {
-    path: "subscriptions",
+    path: 'subscriptions',
     element: (
       <Layout>
         <SubsSample />
       </Layout>
-    ),
-  },
-]);
+    )
+  }
+])
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
